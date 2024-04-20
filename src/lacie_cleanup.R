7e20d9e6db5608cfe2ab5c46696434ceae38666a
thelonius182 <lcavdakker@gmail.com>
2024-04-21 01:40:59 +0200

    rebased using both wordpress and lacie-drive

--- src/lacie_cleanup.R
+++ src/lacie_cleanup.R
@@ -168,3 +168,117 @@ needed_bcs_new.1 <- needed_bcs_new |>
 ss <- sheet_write(ss = "1d8t8ZItwfBpdVrB9lyc83-F8NysdHRDpgnw-TJ9G2ng",
                   sheet = "woj_herhalingen_3.0",
                   data = needed_bcs_new.1)
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# WoJ-replays 4.0
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+qry <- str_replace_all("SELECT distinct
+    po1.post_date as bc_start, 
+    pm1.meta_value as bc_stop, 
+    po1.post_title, 
+    tx1.description, 
+    te1.slug
+FROM
+    wp_posts po1
+        LEFT JOIN
+    wp_term_relationships tr1 ON tr1.object_id = po1.id
+        LEFT JOIN
+    wp_term_taxonomy tx1 ON tx1.term_taxonomy_id = tr1.term_taxonomy_id
+        LEFT JOIN
+    wp_terms te1 ON te1.term_id = tr1.term_taxonomy_id
+        LEFT JOIN
+    wp_postmeta pm1 ON pm1.post_id = po1.id
+WHERE
+    po1.post_type = 'programma' and po1.post_date > '2011-06-01'
+        AND pm1.meta_key = 'pr_metadata_uitzenddatum_end'
+        AND (tx1.taxonomy = 'post_translations' AND tx1.description REGEXP '(s:2:@en@.*s:2:@nl@)|(s:2:@nl@.*s:2:@en@)'
+             OR tx1.taxonomy = 'programma_genre' AND te1.slug REGEXP '__.*-(nl|en)$')
+;", "@", '"')
+
+wp_posts <- dbGetQuery(wp_conn, qry)
+
+wp_posts.1 <- wp_posts |> arrange(bc_start, description, slug)
+wp_posts.2 <- wp_posts.1 |> as_tibble() |> select(-post_title) |> distinct() |> 
+  group_by(bc_start) |> summarise(n_rows = n()) |> ungroup() |> 
+  filter(n_rows %in% c(3, 5, 7))
+
+wp_posts.3 <- wp_posts.1 |> inner_join(wp_posts.2, by = join_by(bc_start)) |> 
+  filter(str_detect(slug, "-nl$")) |> group_by(bc_start) |> mutate(g1 = row_number()) |> ungroup() |> 
+  filter(g1 == 1) |> mutate(post_title = str_to_lower(post_title) |> str_replace_all("&amp;|&", "and" ))
+
+wp_posts.4 <- wp_posts.3 |> 
+  filter(str_detect(post_title, "mazen|jazz piano|vocal|duke|hard bop|groove|three|rata")) |> 
+  filter(bc_start <= "2023-08-16 23:59:59") |> select(c(bc_start:post_title))
+
+cur_ss <- read_sheet("1d8t8ZItwfBpdVrB9lyc83-F8NysdHRDpgnw-TJ9G2ng", sheet = "woj_herhalingen_3.0")
+cur_ss <- cur_ss |> mutate(bc_orig_ymd_new = round_date(bc_orig_ymd_new, unit = "hour"))
+
+cur_ss_unprepped <- cur_ss |> filter(!bc_prepped)
+
+cur_ss_prepped <- cur_ss |> filter(bc_prepped) |> select(bc_orig_ymd_new) |> 
+  mutate(bc_prepped = as.character(bc_orig_ymd_new))
+
+wp_posts.5 <- wp_posts.4 |> anti_join(cur_ss_prepped, join_by(bc_start == bc_prepped)) |> 
+  mutate(post_interval = interval(ymd_hms(bc_start), ymd_hms(paste0(bc_stop, ":00"))))
+
+arch_2011 <- read_delim("C:/cz_salsa/cz_exchange/archief_tot_201106-201406.txt",
+                                        delim = ":", escape_double = FALSE, col_names = FALSE,
+                                        show_col_types = FALSE,
+                                        trim_ws = TRUE) |> as_tibble() |> mutate(file = path_file(X1))
+
+
+arch_2014 <- read_delim("C:/cz_salsa/cz_exchange/Archief_tot_201407-201706.txt",
+                                        delim = ":", escape_double = FALSE, col_names = FALSE,
+                                        show_col_types = FALSE,
+                                        trim_ws = TRUE) |> as_tibble() |> mutate(file = path_file(X1))
+
+
+arch_2017 <- read_delim("C:/cz_salsa/cz_exchange/Archief_tot_201707-202105.txt",
+                                        delim = ":", escape_double = FALSE, col_names = FALSE,
+                                        show_col_types = FALSE,
+                                        trim_ws = TRUE) |> as_tibble() |> mutate(file = path_file(X1))
+
+
+arch_2021 <- read_delim("C:/cz_salsa/cz_exchange/Archief_tot_202105-202310.txt",
+                                        delim = ":", escape_double = FALSE, col_names = FALSE,
+                                        show_col_types = FALSE,
+                                        trim_ws = TRUE) |> as_tibble() |> 
+  mutate(file = path_file(X1), file = str_replace(file, "Logbestand", ""))
+
+arch_tot <- arch_2011 |> bind_rows(arch_2014) |> bind_rows(arch_2017) |> bind_rows(arch_2021) |> 
+  mutate(fn_len = str_length(str_trim(file))) |> filter(fn_len == 17) |> select(file, X2)
+
+arch_tot.1 <- arch_tot |> filter(X2 != "minutes") |> mutate(minutes_raw = parse_number(X2)) |> 
+  filter(minutes_raw > 0 & (minutes_raw %% 60 <= 2 | minutes_raw %% 60 >= 58)) |> 
+  mutate(minutes_rounded = round(minutes_raw, -1),
+         bc_ymd = round_date(ymd_hm(str_replace(file, "\\.m4a", "")), unit = "hour"),
+         bc_interval = interval(start = bc_ymd, end = bc_ymd + minutes(minutes_rounded))) |> 
+  select(file, bc_interval)
+
+arch_tot_intervals <- arch_tot.1$bc_interval |> as.list()
+
+wp_posts.6 <- wp_posts.5 |> mutate(audio_exists = post_interval %within% arch_tot_intervals)
+wp_posts.7 <- wp_posts.6 |> filter(audio_exists) |> select(post_title, bc_start, bc_stop) |> 
+  group_by(post_title) |> mutate(bc_key = row_number()) |> ungroup() |> arrange(post_title, bc_start)
+
+cur_ss_unprepped.1 <- cur_ss_unprepped |> mutate(bc_title = str_replace(bc_title, "&", "and")) |> 
+  group_by(bc_title) |> mutate(bc_key = row_number()) |> ungroup()
+
+cur_ss_unprepped.2 <- cur_ss_unprepped.1 |> left_join(wp_posts.7, join_by(bc_title == post_title, bc_key)) |> 
+  filter(!is.na(bc_start))
+
+cur_ss_unprepped.3 <- cur_ss_unprepped.2 |> 
+  mutate(bc_review = "WoJ LaCie-replays 4.0",
+         bc_orig_ymd_new = ymd_hms(bc_start),
+         bc_orig_id_new = NA_real_,
+         bc_audio_new = str_remove(bc_audio_new, "_\\d{6}$")) |> 
+  select(-c(bc_key:bc_stop))
+
+cur_ss_prepped.1 <- cur_ss |> filter(bc_prepped)
+
+new_ss <- cur_ss_prepped.1 |> bind_rows(cur_ss_unprepped.3) |> arrange(bc_title, bc_woj_ymd) |> 
+  select(bc_woj_ymd, bc_orig_ymd:bc_orig_ymd_new, bc_title, everything())
+
+# + upload to GD ----
+ss <- sheet_write(ss = "1d8t8ZItwfBpdVrB9lyc83-F8NysdHRDpgnw-TJ9G2ng",
+                  sheet = "woj_herhalingen_4.0",
+                  data = new_ss)
