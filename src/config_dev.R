efec7739ff72e923151726d2d9d9dead8192b961
thelonius182 <lcavdakker@gmail.com>
2024-07-13 16:08:06 +0200

    move init steps to config_dev.R and -prd.R

--- src/config_dev.R
+++ src/config_dev.R
@@ -1,7 +1,24 @@
-config <- yaml::read_yaml("config_dev.yaml")
+message("using DEV-config")
 
-woj_gids_creds_dev <- readr::read_rds(config$db_dev_creds)
+pacman::p_load(googledrive, googlesheets4, dplyr, tidyr, lubridate, fs, uuid, gmailr, RPostgres,
+               stringr, yaml, readr, rio, RMySQL, keyring, jsonlite, futile.logger, conflicted)
+
+conflicts_prefer(dplyr::lag, dplyr::lead, dplyr::filter, lubridate::minutes, .quiet = T)
+
+source("src/functions.R", encoding = "UTF-8")
+config <- read_yaml("config_dev.yaml")
+
+woj_gids_creds_dev <- read_rds(config$db_dev_creds)
 db_host <- woj_gids_creds_dev$db_host
 db_user <- woj_gids_creds_dev$db_user
 db_password <- woj_gids_creds_dev$db_password
 db_name <- woj_gids_creds_dev$db_name
+
+lg_ini <- flog.appender(appender.file(config$log_file), name = config$log_slug)
+git_info <- salsa_git_version(getwd())
+flog.info(">>> START", name = config$log_slug)
+flog.info(sprintf("git-branch: %s", git_info$git_branch), name = config$log_slug)
+flog.info(sprintf("  commited: %s", git_info$ts), name = config$log_slug)
+flog.info(sprintf("        by: %s", git_info$by), name = config$log_slug)
+flog.info(sprintf("local repo: %s", git_info$path), name = config$log_slug)
+flog.info(sprintf("  using db: %s", config$wpdb_env), name = config$log_slug)
--- src/config_prd.R
+++ src/config_prd.R
@@ -1,6 +1,23 @@
-config <- yaml::read_yaml("config_prd.yaml")
+message("using PRD-config")
 
-db_host <- keyring::key_get(service = "sql-wpprd_host")
-db_user <- keyring::key_get(service = "sql-wpprd_user")
-db_password <- keyring::key_get(service = "sql-wpprd_pwd")
-db_name <- keyring::key_get(service = "sql-wpprd_db")
+pacman::p_load(googledrive, googlesheets4, dplyr, tidyr, lubridate, fs, uuid, gmailr, RPostgres,
+               stringr, yaml, readr, rio, RMySQL, keyring, jsonlite, futile.logger, conflicted)
+
+conflicts_prefer(dplyr::lag, dplyr::lead, dplyr::filter, lubridate::minutes, .quiet = T)
+
+source("src/functions.R", encoding = "UTF-8")
+config <- read_yaml("config_prd.yaml")
+
+db_host <- key_get(service = "sql-wpprd_host")
+db_user <- key_get(service = "sql-wpprd_user")
+db_password <- key_get(service = "sql-wpprd_pwd")
+db_name <- key_get(service = "sql-wpprd_db")
+
+lg_ini <- flog.appender(appender.file(config$log_file), name = config$log_slug)
+git_info <- salsa_git_version(getwd())
+flog.info(">>> START", name = config$log_slug)
+flog.info(sprintf("git-branch: %s", git_info$git_branch), name = config$log_slug)
+flog.info(sprintf("  commited: %s", git_info$ts), name = config$log_slug)
+flog.info(sprintf("        by: %s", git_info$by), name = config$log_slug)
+flog.info(sprintf("local repo: %s", git_info$path), name = config$log_slug)
+flog.info(sprintf("  using db: %s", config$wpdb_env), name = config$log_slug)
